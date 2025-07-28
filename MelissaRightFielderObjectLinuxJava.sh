a9b30d6a5308c95cffb4422a9374202a60976e6e
Melissa <>
2025-07-28 12:37:00 -0700

    Tested successfully using Melissa data files for 2025.Q3

--- MelissaRightFielderObjectLinuxJava.sh
+++ MelissaRightFielderObjectLinuxJava.sh
@@ -53,7 +53,7 @@ done
 
 
 # ######################### Config ###########################
-RELEASE_VERSION='2025.Q2'
+RELEASE_VERSION='2025.Q3'
 ProductName="RF_DATA"
 
 # Uses the location of the .sh file 
